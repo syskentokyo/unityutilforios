d5181ff5b9e88b9299e2d6da77ee60f1b275aa07
ppengotsu <ppengotsu.github@ppen.info>
2023-02-10 02:06:39 +0900

    Bitcodeオフにする機能

---
+++ Assets/SyskenTLib/UtilForiOS/NoBitCode/Editor/NoBitCodeSettingPostProcessor.cs
@@ -0,0 +1,45 @@
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+
+
+namespace SyskenTLib.UtilForiOS.NoBitcodeEditor
+{
+    public class NoBitCodeSettingPostProcessor 
+    {
+
+        
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+        {
+            
+            if (buildTarget== BuildTarget.iOS)
+            {
+                string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+                PBXProject pbxProject = new PBXProject();
+                pbxProject.ReadFromFile(projectPath);
+                
+                //Main
+                string target = pbxProject.GetUnityMainTargetGuid();
+                pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+
+                //テスト用
+                target = pbxProject.TargetGuidByName(PBXProject.GetUnityTestTargetName());
+                pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+                
+                //Unity Framework
+                target = pbxProject.GetUnityFrameworkTargetGuid();
+                pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+
+
+                pbxProject.WriteToFile(projectPath);
+            }
+        }
+    }
+}
