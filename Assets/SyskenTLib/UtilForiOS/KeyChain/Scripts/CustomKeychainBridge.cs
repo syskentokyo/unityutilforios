e7abf731b4f2557cd9980bc7f91b3f318a260a66
ppengotsu <ppengotsugihub@sysken.tokyo>
2024-05-13 00:52:58 +0900

    iosだけメソッド処理するように変更

--- Assets/SyskenTLib/UtilForiOS/KeyChain/Scripts/CustomKeychainBridge.cs
+++ Assets/SyskenTLib/UtilForiOS/KeyChain/Scripts/CustomKeychainBridge.cs
@@ -8,6 +8,7 @@ namespace SyskenTLib.UtilForiOS.KeyChain
 
     public class CustomKeychainBridge
     {
+#if UNITY_IOS
         [DllImport("__Internal", EntryPoint = "SyskenTlibCustomKeychainBridgeIsExistData")]
         private static extern bool SyskenTlibCustomKeychainBridgeIsExistData(String serviceName,String keyName);
         
@@ -20,28 +21,38 @@ namespace SyskenTLib.UtilForiOS.KeyChain
         [DllImport("__Internal", EntryPoint = "SyskenTlibCustomKeychainBridgeRead")]
         private static extern string SyskenTlibCustomKeychainBridgeRead(String serviceName,String keyName);
 
-
+#endif
 
         public bool IsExistData(String serviceName, String keyName)
         {
+#if UNITY_IOS
             bool isExist = SyskenTlibCustomKeychainBridgeIsExistData(serviceName, keyName);
             return isExist;
+#endif
+            return false;
         }
 
         public void Delete(String serviceName, String keyName)
         {
+            #if UNITY_IOS
             SyskenTlibCustomKeychainBridgeDelete(serviceName, keyName);
+            #endif
         }
         
         
         public void Save(String serviceName, String keyName, String saveValue)
         {
+#if UNITY_IOS
             SyskenTlibCustomKeychainBridgeSave(serviceName, keyName, saveValue);
+#endif
         }
 
         public string Read(String serviceName, String keyName)
         {
+#if UNITY_IOS
             return SyskenTlibCustomKeychainBridgeRead(serviceName, keyName);
+#endif
+            return "";
         }
 
 
