2fd4a49090b6c2de1f96a38b9718ecf13d54e208
ppengotsu <ppengotsugihub@sysken.tokyo>
2025-01-01 21:32:32 +0900

    指定ファイルがなかったときに処理しないように修正

--- Assets/SyskenTLib/UtilForiOS/CustomFirstSplash/Editor/CustomFirstSplashPostProcessor.cs
+++ Assets/SyskenTLib/UtilForiOS/CustomFirstSplash/Editor/CustomFirstSplashPostProcessor.cs
@@ -15,7 +15,7 @@ namespace SyskenTLib.UtilForiOS.CustomFirstSplash.Editor
     {
      
         
-        [PostProcessBuild(1)]
+        [PostProcessBuild(10)]
         public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
         {
 
@@ -33,55 +33,83 @@ namespace SyskenTLib.UtilForiOS.CustomFirstSplash.Editor
                     string projectDirPath =　 System.IO.Directory.GetParent(System.IO.Path.GetDirectoryName (projectPath)).FullName;
                     Debug.Log("ProjectPath "+projectDirPath);
                     
+                    PBXProject pbxProject = new PBXProject();
+                    pbxProject.ReadFromFile(projectPath);
+                    
                     
                     //
                     // SplashのiPhone用背景書き込み
                     //
-                    string splashSceneBackgroundForIphonePortraitPath = System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPhonePortrait.png");
-                    string splashSceneBackgroundForIphoneLandscapePath = System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPhoneLandscape.png");
-                    byte[] splashSceneBackgroundForIphoneBytes = config.iPhoneBackgroundImage.bytes;;
-                    System.IO.File.WriteAllBytes(splashSceneBackgroundForIphonePortraitPath, splashSceneBackgroundForIphoneBytes);
-                    System.IO.File.WriteAllBytes(splashSceneBackgroundForIphoneLandscapePath, splashSceneBackgroundForIphoneBytes);
+                    if (config.iPhoneBackgroundImage != null)
+                    {
+                        string splashSceneBackgroundForIphonePortraitPath =
+                            System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPhonePortrait.png");
+                        string splashSceneBackgroundForIphoneLandscapePath =
+                            System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPhoneLandscape.png");
+                        byte[] splashSceneBackgroundForIphoneBytes = config.iPhoneBackgroundImage.bytes;
+                        ;
+                        System.IO.File.WriteAllBytes(splashSceneBackgroundForIphonePortraitPath,
+                            splashSceneBackgroundForIphoneBytes);
+                        System.IO.File.WriteAllBytes(splashSceneBackgroundForIphoneLandscapePath,
+                            splashSceneBackgroundForIphoneBytes);
+                    }
                     
+
                     //
                     // SplashのiPad用背景書き込み
                     //
-                    string splashSceneBackgroundForIPadPath = System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPad.png");
-                    byte[] splashSceneBackgroundForIPadBytes = config.iPadBackgroundImage.bytes;;
-                    System.IO.File.WriteAllBytes(splashSceneBackgroundForIPadPath, splashSceneBackgroundForIPadBytes);
-                    
-                                        
+                    if (config.iPadBackgroundImage != null)
+                    {
+                        string splashSceneBackgroundForIPadPath =
+                            System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPad.png");
+                        byte[] splashSceneBackgroundForIPadBytes = config.iPadBackgroundImage.bytes;
+                        ;
+                        System.IO.File.WriteAllBytes(splashSceneBackgroundForIPadPath,
+                            splashSceneBackgroundForIPadBytes);
+                    }
+
                     //
                     // SplashのiPone用Storyboard書き込み
                     //
-                    string splashSceneStoryboardForIPhonePath = System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPhone.storyboard");
-                    string splashSceneStoryboardForIPhoneText = config.splashStoryboardIPhone.text;;
-                    System.IO.File.WriteAllText(splashSceneStoryboardForIPhonePath, splashSceneStoryboardForIPhoneText);
+                    if (config.splashStoryboardIPhone != null)
+                    {
+                        string splashSceneStoryboardForIPhonePath =
+                            System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPhone.storyboard");
+                        string splashSceneStoryboardForIPhoneText = config.splashStoryboardIPhone.text;
+                        ;
+                        System.IO.File.WriteAllText(splashSceneStoryboardForIPhonePath,
+                            splashSceneStoryboardForIPhoneText);
+                    }
 
                     //
                     // SplashのiPad用Storyboard書き込み
                     //
-                    string splashSceneStoryboardForIPadPath = System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPad.storyboard");
-                    string splashSceneStoryboardForIPadText = config.splashStoryboardIpad.text;;
-                    System.IO.File.WriteAllText(splashSceneStoryboardForIPadPath, splashSceneStoryboardForIPadText);
+                    if (config.splashStoryboardIpad != null)
+                    {
+                        string splashSceneStoryboardForIPadPath =
+                            System.IO.Path.Combine(projectDirPath, "LaunchScreen-iPad.storyboard");
+                        string splashSceneStoryboardForIPadText = config.splashStoryboardIpad.text;
+                        ;
+                        System.IO.File.WriteAllText(splashSceneStoryboardForIPadPath, splashSceneStoryboardForIPadText);
+                    }
 
-                    
                     //
                     //ロゴ書き込み
                     //
-                    string logoPath = System.IO.Path.Combine(projectDirPath, "appmainlogo.png");
-                    byte[] logoBytes = config.splashLogImage.bytes;;
-                    System.IO.File.WriteAllBytes(logoPath, logoBytes);
                     
-                    //ロゴファイルのパスを追加する
-                    PBXProject pbxProject = new PBXProject();
-                    pbxProject.ReadFromFile(projectPath);
+                    if (config.splashLogImage != null)
+                    {
+                        string logoPath = System.IO.Path.Combine(projectDirPath, "appmainlogo.png");
+                        byte[] logoBytes = config.splashLogImage.bytes;
+                        ;
+                        System.IO.File.WriteAllBytes(logoPath, logoBytes);
 
-                    string appmainlogoGUID = pbxProject.AddFile(logoPath,"/appmainlogo.png", PBXSourceTree.Source );
+                        //ロゴファイルのパスを追加する
+                        string appmainlogoGUID = pbxProject.AddFile(logoPath, "/appmainlogo.png", PBXSourceTree.Source);
+                        string targetGUID = pbxProject.GetUnityMainTargetGuid();
+                        pbxProject.AddFileToBuild(targetGUID, appmainlogoGUID);
+                    }
 
-                    string targetGUID = pbxProject.GetUnityMainTargetGuid();
-                    pbxProject.AddFileToBuild( targetGUID, appmainlogoGUID );
-                    
                     pbxProject.WriteToFile(projectPath);
                     
                     
